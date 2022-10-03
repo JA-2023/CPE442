6db4ab6eb822003e85b545b47cd4e9a2d29eb989
Josh Alderson <92811764+JA-2023@users.noreply.github.com>
2022-10-02 22:32:29 -0700

    Add files via upload

---
+++ lab2/lab2.cpp
@@ -0,0 +1,39 @@
+#include <opencv2/highgui.hpp> 
+#include <unistd.h>
+
+
+using namespace cv;
+
+
+int main(int argc, char* argv[])
+{
+    //char array to hold the file name
+    char file[20];
+    //put command line argument in variable
+    strcat(file,argv[1]);
+    //get file path for the image
+    char path[100];
+    getcwd(path, 100);
+ 
+    //add directory and file to get full path
+    char* final[100] = {strcat(path,"/")};
+    strcat(*final,file);
+
+    //read the image
+    Mat swan = imread(*final);
+
+    //name the window
+    String window = "swan";
+
+    //create the window
+    namedWindow(window);
+
+    //show the image in the window
+    imshow(window, swan);
+    //wait for key press
+    waitKey(0);
+    //destroy window after key is pressed
+    destroyWindow(window);
+
+    return(0);
+}
