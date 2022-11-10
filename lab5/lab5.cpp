bc0abb0eafe5ee5691d6114a813c7c1d7c443533
Josh Alderson <joshua.aldersonee@gmail.com>
2022-11-10 00:14:57 -0800

    fix

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -227,7 +227,7 @@ void *thread_filter(void *args)
         //move the pointer to the correct starting position
         pixel = arguments->frame.data + (start_gray *3*8); //multiply by 3 for RGB and 8 for the vectors
         gray_data = arguments->gray.data + (start_gray * 8); //increment by 8 for the vectors
-        sobel_data = arguments->sobel.data;
+        sobel_data = arguments->sobel.data + (sobel_data * 8);
     
         //for loop
         for(int i = start_gray; i < stop_gray; i++, pixel += 8 * 3, gray_data += 8)
@@ -249,7 +249,7 @@ void *thread_filter(void *args)
             vst1_u8(gray_data, gray_vect);
         }
         //reset gray pointer so sobel works
-        gray_data = arguments->gray.data;
+        gray_data = arguments->gray.data + (start_gray * 8);
 
         for(int i = start_sobel; i < stop_sobel; i++, gray_data += 8, sobel_data += 8)
         {
