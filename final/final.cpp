acd8c7ad62e660f1662acd99d590c4252460f985
JA-2023 <joshua.aldersonee@gmail.com>
2022-11-30 11:46:40 -0800

    took out rezing and frame naming. fixes window size and fps slow down

--- final/final.cpp
+++ final/final.cpp
@@ -146,13 +146,9 @@ int main(int argc, char* argv[])
         frame_counter++;
         //get the time when the frame is captured
         gettimeofday(&start, NULL);
+        
         if(vid_frame.empty())
             break;
-        
-        
-        //resize image to fit on 1920x1080 screen
-        namedWindow("vid_frame", WINDOW_NORMAL);
-        resizeWindow("vid_frame", 1920, 1080);
         
         //display the frame
         imshow("vid_frame", filtered_frame);
